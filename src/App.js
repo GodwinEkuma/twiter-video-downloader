09537783b7622d38bae7c566296fe4404c0745f1
Godwin Ekuma <godwin.ekuma@andela.com>
2019-06-07 05:55:57 +0100

    update the pop up page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src="https://media.giphy.com/media/XGgggKHl4U4hVALmtF/giphy.gif"
+          className="App-logo"
+          alt="logo"
+        />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/GodwinEkuma/twiiter-video-downloader"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Developed by Godwin Ekuma
         </a>
       </header>
     </div>
